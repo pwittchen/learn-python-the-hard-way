e397c39d4259be186b48dd360732f8c54e487a62
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-07-25 17:52:11 +0200

    Updating exercise7.py

--- exercise7.py
+++ exercise7.py
@@ -20,3 +20,6 @@ end12 = "r"
 # watch that comma at the end.  try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+
+#print end1 + end2 + end3 + end4 + end5 + end6
+#print end7 + end8 + end9 + end10 + end11 + end12
