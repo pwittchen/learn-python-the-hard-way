b8acdd9b49995674fc3c531675181966371045fc
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-07-27 18:21:39 +0200

    Adding new exercise files and renaming existing files

--- exercise10.py
+++ exercise10.py
@@ -29,18 +29,18 @@ while True:
 
 # Escape    What it does
 #===========================
-#\\			Backslash ()
-#\'			Single-quote (')
-#\"			Double-quote (")
-#\a			ASCII bell (BEL)
-#\b			ASCII backspace (BS)
-#\f			ASCII formfeed (FF)
+#\\         Backslash ()
+#\'	        Single-quote (')
+#\"         Double-quote (")
+#\a         ASCII bell (BEL)
+#\b         ASCII backspace (BS)
+#\f         ASCII formfeed (FF)
 #\n			ASCII linefeed (LF)
-#\N{name}	Character named name in the Unicode database (Unicode only)
-#\r ASCII	Carriage Return (CR)
-#\t ASCII	Horizontal Tab (TAB)
-#\uxxxx		Character with 16-bit hex value xxxx (Unicode only)
-#\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (Unicode only)
-#\v			ASCII vertical tab (VT)
-#\ooo		Character with octal value ooo
-#\xhh		Character with hex value hh
+#\N{name}   Character named name in the Unicode database (Unicode only)
+#\r ASCII   Carriage Return (CR)
+#\t ASCII   Horizontal Tab (TAB)
+#\uxxxx	    Character with 16-bit hex value xxxx (Unicode only)
+#\Uxxxxxxxx Character with 32-bit hex value xxxxxxxx (Unicode only)
+#\v         ASCII vertical tab (VT)
+#\ooo       Character with octal value ooo
+#\xhh       Character with hex value hh
