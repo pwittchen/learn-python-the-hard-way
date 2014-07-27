df7e4b5a0a09f92b4fc56117ef4609dfc45dd522
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-07-27 18:20:11 +0200

    Updating formatting of exercise10.py file

--- exercise10.py
+++ exercise10.py
@@ -27,20 +27,20 @@ while True:
 
 # Escape Sequences
 
-# Escape   |	What it does
+# Escape    What it does
 #===========================
-#\\				Backslash ()
-#\'				Single-quote (')
-#\"				Double-quote (")
-#\a				ASCII bell (BEL)
-#\b				ASCII backspace (BS)
-#\f				ASCII formfeed (FF)
-#\n				ASCII linefeed (LF)
-#\N{name}		Character named name in the Unicode database (Unicode only)
-#\r ASCII		Carriage Return (CR)
-#\t ASCII		Horizontal Tab (TAB)
-#\uxxxx			Character with 16-bit hex value xxxx (Unicode only)
-#\Uxxxxxxxx		Character with 32-bit hex value xxxxxxxx (Unicode only)
-#\v				ASCII vertical tab (VT)
-#\ooo			Character with octal value ooo
-#\xhh			Character with hex value hh
+#\\			Backslash ()
+#\'			Single-quote (')
+#\"			Double-quote (")
+#\a			ASCII bell (BEL)
+#\b			ASCII backspace (BS)
+#\f			ASCII formfeed (FF)
+#\n			ASCII linefeed (LF)
+#\N{name}	Character named name in the Unicode database (Unicode only)
+#\r ASCII	Carriage Return (CR)
+#\t ASCII	Horizontal Tab (TAB)
+#\uxxxx		Character with 16-bit hex value xxxx (Unicode only)
+#\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (Unicode only)
+#\v			ASCII vertical tab (VT)
+#\ooo		Character with octal value ooo
+#\xhh		Character with hex value hh
