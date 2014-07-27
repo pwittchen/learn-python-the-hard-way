2c6f44e3a1bc33a0efab8debd2bb9815e3d4e79d
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-07-27 19:09:17 +0200

    updating exercise13.py

--- exercise13.py
+++ exercise13.py
@@ -8,3 +8,6 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+# from terminam window run e.g.:
+# python exercise13.py a b c
