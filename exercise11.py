83919c522e52574b385bc1b17b0bd1cd1b0c258e
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-07-27 18:33:31 +0200

    Adding exercise11.py

---
+++ exercise11.py
@@ -0,0 +1,10 @@
+# Exercise 11: Asking questions and reading input
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you (in cm)?",
+height = raw_input()
+print "How much do you weigh (in kg)?",
+weight = raw_input()
+
+print "So, you're %r-years old, %r cm tall and %r kg heavy." % (age, height, weight)
