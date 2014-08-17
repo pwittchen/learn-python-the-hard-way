f05e7c49192c58e2bddc2d57c2f63c707f6cbf93
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-08-17 21:20:24 +0200

    Adding resolving exercise17

---
+++ exercise17.py
@@ -0,0 +1,27 @@
+# Exercise 16: More files
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+# we could do these two on one line too, how?
+# indata = open(from_file).read() # probably, like that
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
