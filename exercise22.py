b3ad239c383a31396c6d6c3f356c2e8d2b799a43
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-12-07 15:11:27 +0100

    Adding exercise 22 and 23

---
+++ exercise22.py
@@ -0,0 +1,3 @@
+# Exercise 22: What Do You Know So Far?
+# No code
+# Read: http://learnpythonthehardway.org/book/ex22.html
---
+++ exercise23.py
@@ -0,0 +1,3 @@
+# Exercise 23: Read Some Code
+# No code
+# Read: http://learnpythonthehardway.org/book/ex23.html
