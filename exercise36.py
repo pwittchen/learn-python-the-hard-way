fb41ee026ce52c17e3b59975c97cf23cce163dfb
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-12-07 17:59:41 +0100

    adding exercise 36 and 37

---
+++ exercise36.py
@@ -0,0 +1,3 @@
+# Exercise 36: Designing and debugging
+# No code
+# Read: http://learnpythonthehardway.org/book/ex36.html
---
+++ exercise37.py
@@ -0,0 +1,3 @@
+# Exercise 37: Symbol review
+# No code
+# Read: http://learnpythonthehardway.org/book/ex37.html
