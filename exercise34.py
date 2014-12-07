1fa7e796f7b4e95306b1504f9747214fd2d8f03e
Piotr Wittchen <piotr@wittchen.biz.pl>
2014-12-07 17:53:12 +0100

    adding exercise 34

---
+++ exercise34.py
@@ -0,0 +1,6 @@
+# Exercise 34: Accessing elements of Lists
+
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+print "animals =", animals
+print "animals[0] =", animals[0]
+print "animals[2] =", animals[2]
