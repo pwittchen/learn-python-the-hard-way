f1d9f8b33114138b8e62eb293fee411b028ddb1b
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-02-01 20:52:11 +0100

    adding web game for exercise52

---
+++ exercises/exercise52/game/bin/app.py
@@ -0,0 +1,49 @@
+import web
+import map
+
+urls = (
+  '/game', 'GameEngine',
+  '/', 'Index',
+)
+
+app = web.application(urls, globals())
+
+# little hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+    store = web.session.DiskStore('sessions')
+    session = web.session.Session(app, store,
+                                  initializer={'room': None})
+    web.config._session = session
+else:
+    session = web.config._session
+
+render = web.template.render('templates/', base="layout")
+
+
+class Index(object):
+    def GET(self):
+        # this is used to "setup" the session with starting values
+        session.room = map.START
+        web.seeother("/game")
+
+
+class GameEngine(object):
+
+    def GET(self):
+        if session.room:
+            return render.show_room(room=session.room)
+        else:
+            # why is there here? do you need it?
+            return render.you_died()
+
+    def POST(self):
+        form = web.input(action=None)
+
+        # there is a bug here, can you fix it?
+        if session.room and form.action:
+            session.room = session.room.go(form.action)
+
+        web.seeother("/game")
+
+if __name__ == "__main__":
+    app.run()
