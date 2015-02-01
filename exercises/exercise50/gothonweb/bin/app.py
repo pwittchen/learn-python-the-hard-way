602d10ac0c44b0e432af17d70cc72f252090d562
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-02-01 19:56:05 +0100

    adding bin directories to the repository for sake of consistency

---
+++ exercises/exercise50/gothonweb/bin/app.py
@@ -0,0 +1,16 @@
+import web
+
+urls = (
+  '/', 'index'
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates/')
+
+class index:
+  def GET(self):
+    greeting = "Hello World"
+    return render.index(greeting = greeting)
+
+if __name__ == "__main__":
+  app.run()
